9767987989e3e0bba812a797b4aa57cf58f81910
Wenodh <vinodkumara3@gmail.com>
2024-03-11 23:13:42 +0530

    intro setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [{ protocol: 'https', hostname: 'images.app.goo.gl' }],
+        remotePatterns: [
+            { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+        ],
     },
 };
 
