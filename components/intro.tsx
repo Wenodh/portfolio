3c6280d4c2830173d4a8399a9298b87c3e66a8dc
Wenodh <vinodkumara3@gmail.com>
2024-03-13 01:08:36 +0530

    experience

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,9 +8,11 @@ import { FaGithubSquare } from 'react-icons/fa';
 import { HiDownload } from 'react-icons/hi';
 import Link from 'next/link';
 import { useSectionInView } from '@/lib/hooks';
+import { useActiveSectionContext } from '@/context/activeSectionContextProvider';
 
 export default function Intro() {
     const { ref } = useSectionInView("Home");
+    const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
     return (
         <section ref={ref} className="mb-28 max-w-[50rem] text-center scroll-mt-[100rem]" id="home">
             <div className="flex items-center justify-center">
@@ -71,10 +73,10 @@ export default function Intro() {
                 <Link
                     href="#contact"
                     className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-                    //   onClick={() => {
-                    //     setActiveSection("Contact");
-                    //     setTimeOfLastClick(Date.now());
-                    //   }}
+                      onClick={() => {
+                        setActiveSection("Contact");
+                        setTimeOfLastClick(Date.now());
+                      }}
                 >
                     Contact me here{' '}
                     <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
